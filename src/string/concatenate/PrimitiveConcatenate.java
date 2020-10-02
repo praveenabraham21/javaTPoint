2cdbe277e7dfe8f5f6b0f708ab21ab26e1bc19be
Praveen <praveenabraham2146@gmail.com>
2020-10-02 20:13:16 +0530

    string practice

---
+++ src/string/concatenate/PrimitiveConcatenate.java
@@ -0,0 +1,14 @@
+package string.concatenate;
+
+public class PrimitiveConcatenate {
+
+	public static void main(String[] args) {
+
+		String a = "Sachin";
+		String b = 9 + 9 + a + "Tendulkar";
+
+		System.out.println(b);
+
+	}
+
+}
